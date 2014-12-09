60545043b3acc173fed9fa146f0737c80b99cd48
翰文 <eward.song@gmail.com>
2014-12-09 22:44:54 +0800

    change code

--- generator/g.js
+++ generator/g.js
@@ -23,11 +23,11 @@ function* c(){
 var gen = a();
 
 gen.next().value(function(err, ret){
-  var next = gen.next(ret); // ret value eql to d
+  var next = gen.next(ret).value; // ret value eql to d
 
   // c is generator, use next to run
-  next.value.next().value(function(err, ret){
-    var a = next.value.next(ret);
+  next.next().value(function(err, ret){
+    var a = next.next(ret);
     gen.next(a.value);
   });
 });
