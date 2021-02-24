6f8dce82058471990af7b2f535efc92ed2fb90d1
shepherdwind <eward.song@gmail.com>
2021-02-24 23:16:07 +0800

    feat: 增加 NestedIterator 问题解法

--- leecode/iterator.ts
+++ leecode/iterator.ts
@@ -1,6 +1,18 @@
+type List = number | List[];
 class NestedInteger {
   private list: NestedInteger[];
   private value: number;
+
+  static from(list: List[]) {
+    return list.map(o => {
+      if (typeof o === 'number') {
+        return new NestedInteger(o);
+      }
+      const item = new NestedInteger();
+      item.list = NestedInteger.from(o);
+      return item;
+    });
+  }
 
   constructor(value?: number) {
     if (Number.isInteger(value)) {
@@ -25,15 +37,58 @@ class NestedInteger {
     return this.isInteger() ? null : this.list;
   }
 }
+class LinkList {
+  val: number;
+  next: LinkList | null;
+  constructor(val?: number, next?: LinkList| null) {
+    this.val = val;
+    this.next = next || null;
+  }
+}
+
+function transForm(nestedList: NestedInteger[]): LinkList {
+  if (!nestedList || !nestedList.length) {
+    return null;
+  }
+  const item = nestedList.shift();
+  let root: LinkList;
+  if (item.isInteger()) {
+    root = new LinkList(item.getInteger());
+  } else {
+    root = transForm(item.getList());
+  }
+
+  if (!root) {
+    return transForm(nestedList);
+  }
+
+  let end = root;
+  while (end.next) {
+    end = end.next;
+  }
+  end.next = transForm(nestedList);
+  return root;
+}
 
 class NestedIterator {
+  private linkList: LinkList;
   constructor(nestedList: NestedInteger[]) {
+    this.linkList = transForm(nestedList);
   }
 
   hasNext(): boolean {
+    return this.linkList !== null;
   }
 
   next(): number {
+    const item = this.linkList;
+    this.linkList = item.next;
+    return item.val;
   }
 }
+
+(() => {
+  const data = NestedInteger.from([[1,2],3,[4,5]]);
+  console.log(transForm(data));
+})();
 
