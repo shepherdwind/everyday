5cf8006555cb6ba1e72285783f8fe6b113b549c9
shepherdwind <eward.song@gmail.com>
2021-03-02 08:02:16 +0800

    feat: add equationsPossible

--- leecode/array.ts
+++ leecode/array.ts
@@ -56,8 +56,86 @@ function markIt(board: string[][], i: number, j: number) {
     markIt(board, i, j + 1);
   }
 }
+
+class UF {
+  private parents: {
+    [key: string]: string;
+  } = {};
+
+  union(p: string, q: string) {
+    if (this.connected(p, q)) {
+      return true;
+    }
+
+    const pp = this.parents[p];
+    const qp = this.parents[q];
+    if (!pp && !qp) {
+      this.parents[p] = q;
+      this.parents[q] = q;
+      return true;
+    }
+    if (pp && qp) {
+      this.parents[pp] = this.getRoot(qp);
+    }
+
+    if (!pp && qp) {
+      this.parents[p] = qp;
+    }
+
+    if (pp && !qp) {
+      this.parents[q] = pp;
+    }
+    return true;
+  }
+
+  private getRoot(item: string) {
+    let p = this.parents[item];
+    while (p && p !== this.parents[p]) {
+      p = this.parents[p];
+    }
+    return p;
+  }
+
+  connected(p: string, q: string) {
+    const rootP = this.getRoot(p);
+    if (!rootP) {
+      return false;
+    }
+    return rootP === this.getRoot(q);
+  }
+}
+
+function equationsPossible(equations: string[]): boolean {
+  const uf = new UF();
+  const unEquations: Array<[string, string]> = [];
+  for (const item of equations) {
+    const p = item[0];
+    const q = item[3];
+    const eq = item.slice(1, 3) === '==';
+    // 等式，可以连接成功
+    if (eq && !uf.union(p, q)) {
+      return false;
+    }
+    // 不等式，不能有过关联关系
+    if (!eq) {
+      unEquations.push([p, q])
+    }
+  }
+  for (const item of unEquations) {
+    if (uf.connected(item[0], item[1]) || item[0] === item[1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 (() => {
   const board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]];
   solve(board);
   console.log(board);
+});
+
+(() => {
+  const ret = equationsPossible(["a==b","b!=c","c==a"]);
+  console.log(ret);
 })();
