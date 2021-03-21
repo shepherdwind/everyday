9e2000efe2a5343aa3e7a66a59d8cdbd3ebce64d
shepherdwind <eward.song@gmail.com>
2021-03-22 02:07:26 +0800

    chore: add quick sort

--- cs/sort.ts
+++ cs/sort.ts
@@ -1,3 +1,4 @@
+import assert from 'assert';
 function selectionSort(data: number[]) {
   for (let i = 0; i < data.length; i++) {
     let min = i;
@@ -35,6 +36,36 @@ function shell_sort(data: number[]) {
   return data;
 }
 
+function partition(data: number[], start: number, end: number) {
+  let i = Math.floor(Math.random() * (end - start + 1)) + start;
+
+  for (let j = 0; j < data.length; j++) {
+    const needExchange = (data[j] > data[i] && j < i) || (data[j] < data[i] && j > i);
+    if (needExchange) {
+      exchange(data, j, i);
+      i = j;
+    }
+  }
+  return i;
+}
+
+function quick_sort(data: number[], start = 0, end = data.length - 1) {
+  if (end - start < 1) {
+    return data;
+  }
+
+  if (end - start === 1) {
+    if (data[start] > data[end]) {
+      exchange(data, start, end);
+    }
+    return;
+  }
+  const i = partition(data, start, end);
+  quick_sort(data, start, i - 1);
+  quick_sort(data, i + 1, end);
+  return data;
+}
+
 function exchange(data: number[], a: number, b: number) {
   const tmp = data[a];
   data[a] = data[b];
@@ -49,3 +80,6 @@ function exchange(data: number[], a: number, b: number) {
   const ret3 = shell_sort([1, 4, 3, 2, 6, 9, 7, 8]);
   console.log(ret3);
 })();
+
+
+assert.deepStrictEqual(quick_sort([3, 1, 2, 5, 6, 4]), [1, 2, 3, 4, 5, 6])
